f80301bfeb8620c5c9d38a7c73421fc703810507
Graeme Hill <graemekh@gmail.com>
2018-04-19 22:59:02 -0700

    runner sends out /start and /end API calls
    For now the response from /start is just ignored. May also need to
    restructure the code and make better error handling.

--- rules/notify.go
+++ rules/notify.go
@@ -1,13 +1,63 @@
 package rules
 
-import "github.com/battlesnakeio/engine/controller/pb"
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+
+	"github.com/battlesnakeio/engine/controller/pb"
+	log "github.com/sirupsen/logrus"
+)
 
 // NotifyGameStart sends the /start requests to all the snakes
-func NotifyGameStart(game *pb.Game) {
+func NotifyGameStart(game *pb.Game, frame *pb.GameTick) {
+	netClient := createClient(200 * time.Millisecond)
 
+	for _, s := range frame.Snakes {
+		req := buildSnakeRequest(game, frame, s.ID)
+		data, err := json.Marshal(req)
+
+		if err != nil {
+			log.WithError(err).Errorf("error while marshaling snake request: %s", s.ID)
+			// resp <- &SnakeUpdate{
+			// 	Snake: snake,
+			// 	Err:   err,
+			// }
+			return
+		}
+
+		buf := bytes.NewBuffer(data)
+		netClient.Post(getURL(s.URL, "start"), "application/json", buf)
+	}
+
+	// if err != nil {
+	// 	log.WithError(err).Errorf("error while querying %s for move", snake.ID)
+	// 	resp <- &SnakeUpdate{
+	// 		Snake: snake,
+	// 		Err:   err,
+	// 	}
+	// 	return
+	// }
 }
 
 // NotifyGameEnd sends the /end requests to all the snakes
-func NotifyGameEnd(game *pb.Game) {
+func NotifyGameEnd(game *pb.Game, frame *pb.GameTick) {
+	netClient := createClient(200 * time.Millisecond)
 
+	for _, s := range frame.Snakes {
+		req := buildSnakeRequest(game, frame, s.ID)
+		data, err := json.Marshal(req)
+
+		if err != nil {
+			log.WithError(err).Errorf("error while marshaling snake request: %s", s.ID)
+			// resp <- &SnakeUpdate{
+			// 	Snake: snake,
+			// 	Err:   err,
+			// }
+			return
+		}
+
+		buf := bytes.NewBuffer(data)
+		netClient.Post(getURL(s.URL, "end"), "application/json", buf)
+	}
 }
