013f10f17eb0eda4e2290f97460234391cb58b44
Daniel Steuernol <dlsteuer@gmail.com>
2018-05-20 10:03:33 -0700

    start changing to use GameStatus type

--- rules/status.go
+++ rules/status.go
@@ -1,12 +1,14 @@
 package rules
 
+type GameStatus string
+
 var (
 	// GameStatusStopped represents a stopped game
-	GameStatusStopped = "stopped"
+	GameStatusStopped GameStatus = "stopped"
 	// GameStatusRunning represents a running game
-	GameStatusRunning = "running"
+	GameStatusRunning GameStatus = "running"
 	// GameStatusError represents a game that ended because of an error
-	GameStatusError = "error"
+	GameStatusError GameStatus = "error"
 	// GameStatusComplete represents a game that is done
-	GameStatusComplete = "complete"
+	GameStatusComplete GameStatus = "complete"
 )
