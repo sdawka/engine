2d8d30d68645c7b28f37bb6c0e28ab47541c65b2
Daniel Steuernol <dlsteuer@gmail.com>
2019-02-19 14:59:40 -0800

    enable pprof (#91)

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"log"
 	"math/rand"
 	"time"
+
+	"net/http"
+	_ "net/http/pprof"
 
 	"github.com/battlesnakeio/engine/cmd/engine/commands"
 )
 
 func main() {
+
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
 	rand.Seed(time.Now().UnixNano())
 	commands.Execute()
 }
